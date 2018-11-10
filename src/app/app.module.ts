67b2f9b619fdadb7ab5305422ce3b8da8ca100ac
JavierDevoto <javierdevoto96@gmail.com>
2018-11-10 16:25:01 -0300

    subiendoo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PedidoComponent } from './pedido/pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PedidoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
