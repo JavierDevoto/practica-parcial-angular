1cee1f2c5ea880520af32186b4df45ec3ccc723e
JavierDevoto <javierdevoto96@gmail.com>
2018-11-10 17:53:49 -0300

    va

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import 'hammerjs';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PedidoComponent } from './pedido/pedido.component';
+import { ContenedorComponent } from './contenedor/contenedor.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PedidoComponent
+    PedidoComponent,
+    ContenedorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
